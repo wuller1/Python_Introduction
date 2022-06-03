55ff6749a18cb369519871e94a5947cf211d3384
Сизов Иван Сергеевич <PCH9_SizovIS@dvgd.oao.rzd>
2022-06-03 13:01:36 +1000

    assignments are done

--- Week 1/3.py
+++ Week 1/3.py
@@ -1,9 +1,14 @@
-num = input("Введите положительное число: ")
+# 3. Узнайте у пользователя число n. Найдите сумму чисел n + nn + nnn.
+# Например, пользователь ввёл число 3. Считаем 3 + 33 + 333 = 369.
+try:
+    num = input("Введите положительное число: ")
 
-num1 = int(num)
-num2 = int(num + num)
-num3 = int(num + num + num)
+    num1 = int(num)
+    num2 = int(num + num)
+    num3 = int(num + num + num)
 
-num_sum = num1 + num2 + num3
+    num_sum = num1 + num2 + num3
 
-print(num_sum)
+    print(num_sum)
+except ValueError:
+    print("Введены данные не в числовом формате")
