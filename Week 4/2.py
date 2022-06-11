4fa25333d500ccef3dd5175d2a24eb55d9852255
Ivan Sizov <wuller1@gmail.com>
2022-06-11 23:32:53 +1000

    started to solve second assignment

--- Week 4/2.py
+++ Week 4/2.py
@@ -4,4 +4,20 @@
 # Для его формирования используйте генератор.
 # Пример исходного списка: [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55].
 # Результат: [12, 44, 4, 10, 78, 123].
+data = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
 
+
+def greater_than_previous(lst):
+    previous = None
+    new_list = []
+    for el in lst:
+        if previous is None:
+            previous = el
+            continue
+        elif el > previous:
+            new_list.append(el)
+        previous = el
+    return new_list
+
+
+print(greater_than_previous(data))
