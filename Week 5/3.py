2c45ddc0f0359d429c8b592b3bfff48e54309a04
= <=>
2022-06-15 01:11:06 +1000

    solved third assignment

--- Week 5/3.py
+++ Week 5/3.py
@@ -4,3 +4,27 @@
 # Пример файла:
 # Иванов 23543.12
 # Петров 13749.32
+from statistics import mean
+
+
+def less_than_twenty_thousand(filename: str) -> None:
+    salary_dict = {}
+    filtered_dict = {}
+    with open(filename, encoding='utf-8') as file:
+        for line in file:
+            data = line.split(" ")
+            family_name = data[0]
+            salary = float(data[1].rstrip())
+            salary_dict[family_name] = salary
+    for second_name, salary_data in salary_dict.items():
+        if salary_data < 20000:
+            filtered_dict[second_name] = salary_data
+
+    print("Зарабатывают менее 20000 рублей: ")
+    for second_name, salary_data in filtered_dict.items():
+        print(second_name)
+    print("*" * 25)
+    print(f"Средняя заработная плата сотрудников на предприятии: {round(mean(salary_dict.values()), 2)} рублей")
+
+
+less_than_twenty_thousand("text3.txt")
