108937aadf90f7b472017f9178b1d1b54b36c5c4
= <=>
2022-06-15 22:46:41 +1000

    solved fourth assignment

--- Week 5/4.py
+++ Week 5/4.py
@@ -6,3 +6,28 @@
 # Напишите программу, открывающую файл на чтение и считывающую построчно данные.
 # При этом английские числительные должны заменяться на русские. Новый блок строк должен
 # записываться в новый текстовый файл.
+
+filename = "text4.txt"
+filename_out = "text4-out.txt"
+russian_dict = {
+                "One": "Один",
+                "Two": "Два",
+                "Three": "Три",
+                "Four": "Четыре"
+                }
+
+
+def read_file_change_numeric(input_file: str, output_file: str) -> None:
+    translated_list = []
+    with open(filename, encoding="UTF-8") as input_text:
+        for line in input_text:
+            line_list = line.split(" ")
+            line_list[0] = russian_dict[line_list[0]]
+            out_line = " ".join(line_list)
+            translated_list.append(out_line)
+
+            with open(filename_out, "w", encoding="UTF-8") as out_file:
+                out_file.writelines(translated_list)
+
+
+read_file_change_numeric(filename, filename_out)
