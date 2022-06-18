0073a32d92be1b1c7b396835719b2b08665abfb5
Ivan Sizov <wuller1@gmail.com>
2022-06-18 23:56:55 +1000

    solved second assignment

--- Week 6/2.py
+++ Week 6/2.py
@@ -1,4 +1,4 @@
-# . Реализовать класс Road (дорога).
+# 2. Реализовать класс Road (дорога).
 # определить атрибуты: length (длина), width (ширина);
 # значения атрибутов должны передаваться при создании экземпляра класса;
 # атрибуты сделать защищёнными;
@@ -7,3 +7,19 @@
 # толщиной в 1 см*число см толщины полотна;
 # проверить работу метода.
 # Например: 20 м*5000 м*25 кг*5 см = 12500 т.
+
+class Road(object):
+    coefficient = 25
+
+    def __init__(self, length, width, height):
+        self._length = length
+        self._width = width
+        self._height = height
+
+    def calculate_mass(self):
+        return self._length * self._width * self._height * self.coefficient
+
+
+if __name__ == "__main__":
+    road = Road(5000, 20, 5)
+    print(road.calculate_mass())
