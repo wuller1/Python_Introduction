fb481629c2ecd94bad4ce9d111ccd0f64640c6c6
= <wuller1@gmail.com>
2022-06-20 10:41:26 +1000

    finished all assignments

--- Week 6/5.py
+++ Week 6/5.py
@@ -4,3 +4,44 @@
 # в каждом классе реализовать переопределение метода draw. Для каждого класса метод должен
 # выводить уникальное сообщение;
 # создать экземпляры классов и проверить, что выведет описанный метод для каждого экземпляра.
+
+class Stationery(object):
+
+    def __init__(self, title):
+        self.title = title
+
+    @staticmethod
+    def draw():
+        print("Запуск отрисовки")
+
+
+class Pen(Stationery):
+
+    @staticmethod
+    def draw():
+        print("Рисую ручкой")
+
+
+class Pencil(Stationery):
+
+    @staticmethod
+    def draw():
+        print("Рисую карандашом")
+
+
+class Handle(Stationery):
+
+    @staticmethod
+    def draw():
+        print("Рисую маркером")
+
+
+if __name__ == "__main__":
+    stationery = Stationery("Канцелярская принадлежность")
+    pen = Pen("Ручка")
+    pencil = Pencil("Карандаш")
+    handle = Handle("Маркер")
+    stationery.draw()
+    pen.draw()
+    pencil.draw()
+    handle.draw()
