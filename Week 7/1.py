90ba0f96c43ca79f8711ecf5413bc2e90e6fd611
= <wuller1@gmail.com>
2022-06-29 11:46:21 +1000

    solved first assignment

--- Week 7/1.py
+++ Week 7/1.py
@@ -14,3 +14,43 @@
 #
 # Подсказка: сложение элементов матриц выполнять поэлементно — первый элемент первой строки первой
 # матрицы складываем с первым элементом первой строки второй матрицы и т.д.
+
+class Matrix(object):
+    def __init__(self, *args):
+        self.args = args
+        length_previous = len(args[0])
+        for arg in args:
+            if len(arg) != length_previous:
+                raise ValueError("Количество элементов должно совпадать")
+
+    def __str__(self):
+        stringify = ""
+        for el in self.args:
+            stringify += f"\n{el}"
+        return stringify
+
+    def __add__(self, other):
+        row_number = 0
+        column_number = 0
+        added_matrix = []
+
+        if len(self.args) == len(other.args) and len(self.args[0]) == len(other.args[0]):
+            while 0 <= row_number < len(self.args):
+                row_temp = []
+                while 0 <= column_number < len(self.args[0]):
+                    row_temp.append(self.args[row_number][column_number] + other.args[row_number][column_number])
+                    column_number += 1
+                column_number = 0
+                row_number += 1
+                added_matrix.append(row_temp)
+
+        else:
+            raise ValueError("Размеры матриц для сложения должны совпадать")
+        return tuple(added_matrix)
+
+
+matrix1 = Matrix([3, 5, 5, 3], [8, 3, 7, 1])
+matrix2 = Matrix([4, 5, 6, 7], [7, 8, 9, 10])
+matrix3 = Matrix(*(matrix1 + matrix2))
+print(str(matrix3))
+print(str(matrix1))
